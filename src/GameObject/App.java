fa4421b89c786614edce574b4dcc40fe86c721f6
huyonezeno <90764815+huyonezeno@users.noreply.github.com>
2024-10-31 07:20:27 +0700

    Create App.java

---
+++ src/GameObject/App.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication11;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 500;
+        int boardHeight = 500;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        // frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
