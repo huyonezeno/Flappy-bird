5d3e0a79b7bb83a0a78ae08654519f3fd4e50963
huyonezeno <90764815+huyonezeno@users.noreply.github.com>
2024-11-12 20:58:50 +0700

    Update Bird.java

--- src/GameObject/Bird.java
+++ src/GameObject/Bird.java
@@ -2,43 +2,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package GameObject;
-import java.awt.Rectangle;
+package javaapplication11;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.event.KeyEvent;
 
 public class Bird {
-    private float BirdX;
-    private float BirdY;
-    private Rectangle BirdSize;
-    private float g;
-    private boolean GameOver;
-
-    // Default constructor
-    public Bird() {
-        this.GameOver = false;
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+    public boolean dead;
+    public int jumpDelay;
+    public Image img;
+    
+    public Bird(int x, int y, int width, int height,Image img) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.img = img;
     }
-
-    // Parameterized constructor
-    public Bird(float BirdX, float BirdY, Rectangle BirdSize, float g) {
-        this.BirdX = BirdX;
-        this.BirdY = BirdY;
-        this.BirdSize = BirdSize;
-        this.g = g;
-        this.GameOver = false;
+    public Bird(Image img){
+        this.img=img;
     }
-
-    // Bird jumps up
-    public void Jump() {
-        // Logic for jumping
-    }
-
-    // Bird moves down
-    public void MvDown() {
-        // Logic for moving down
-    }
-
-    // Set the size of the bird
-    public void SetBirdSize() {
-        // Logic for setting bird size
-    }
+    
 }
 
