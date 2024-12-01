6e14f7529208652da138764dabccc446e02f3543
huyonezeno <90764815+huyonezeno@users.noreply.github.com>
2024-12-01 20:37:12 +0700

    Update Pipe.java

--- src/GameObject/Pipe.java
+++ src/GameObject/Pipe.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaapplication11;
 
 import java.lang.*;
@@ -18,17 +14,24 @@ import java.awt.event.KeyEvent;
 public class Pipe {
     public int x;
     public int y;
-    public int height;
-    public int width;
+    public static int heightPipe = 300;
+    public static int widthPipe = 70;
     public Image img;
     public boolean passed=false;
-
-    public Pipe(int x, int y, int width,int height,Image img) {
+    public static int startingPipeX = 500;
+    public static int startingPipeY = 0;
+    private static int velocityX = -4;
+    public boolean canAwardPoint = true;
+    
+    public Pipe(int x, int y,Image img) {
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height=height;
         this.img =img;
+    }
+    
+    // di chuyển các ccột
+    public void  movePipe(){
+       x+=velocityX; 
     }
     
 }
